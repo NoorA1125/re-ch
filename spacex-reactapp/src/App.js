46722187c794ad6c20e667799f0059b0de6b6a6e
Noor Abdikadir <nooromar305@gmail.com>
2021-07-07 04:26:43 -0700

    Updated cards to match API name, description, and date. Image is next

--- spacex-reactapp/src/App.js
+++ spacex-reactapp/src/App.js
@@ -1,4 +1,5 @@
-import {Component} from 'react';
+import { Component } from 'react';
+import 'bulma/css/bulma.min.css';
 import './App.css';
 
 class App extends Component {
@@ -12,20 +13,46 @@ class App extends Component {
 
   componentDidMount() {
     fetch("https://api.spacexdata.com/v4/rockets")
-      .then(res => res.json()) 
-      .then(rockets => this.setState({rockets: rockets}))
+      .then(res => res.json())
+      .then(rockets => this.setState({ rockets: rockets }))
   }
 
   render() {
-    
-  return (
-    <div className="App">
-      {this.state.rockets.map((rocket) =>(
-      <h1 key={rocket.id}>{rocket.name}</h1>
-      ))}
-    </div>
-  );
-}
+
+    return (
+      <div className="container">
+        <div className="row">
+          {this.state.rockets.map((rocket) => (
+          <div class="card" key={rocket.id}>
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">{rocket.name}</p>
+                </div>
+              </div>
+
+              <div class="content">
+                {rocket.description}
+                <br />
+                <time datetime="2016-1-1">First Flight: {rocket.first_flight}</time>
+              </div>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
