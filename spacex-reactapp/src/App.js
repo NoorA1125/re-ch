0cd9ad8d746b54570a8a892cd521d6d533bf154b
Noor Abdikadir <nooromar305@gmail.com>
2021-07-07 03:13:28 -0700

    testing api call

--- spacex-reactapp/src/App.js
+++ spacex-reactapp/src/App.js
@@ -1,13 +1,51 @@
+import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      message: "Hello React"
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.spacexdata.com/v4/rockets")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.message}
         </p>
         <a
           className="App-link"
@@ -20,6 +58,8 @@ function App() {
       </header>
     </div>
   );
+}
+}
 }
 
 export default App;
